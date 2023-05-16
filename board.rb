51d777a2154373cfb76d6c4b043ce9cbc48967d2
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-16 13:17:13 +1000

    Add winning combinations and logic to find if there is a winner

--- board.rb
+++ board.rb
@@ -2,8 +2,14 @@
 
 # class representing the tic-tac-toe game board
 class Board
-  def initialize
+  attr_reader :board
+
+  def initialize # rubocop:disable Metrics/AbcSize
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @win = [[@board[0], board[1], board[2]], [@board[3], board[4], board[5]],
+            [@board[6], board[7], board[8]], [@board[0], board[3], board[6]],
+            [@board[1], board[4], board[7]], [@board[2], board[5], board[8]],
+            [@board[0], board[4], board[8]], [@board[2], board[5], board[8]]]
   end
 
   def print_board
@@ -16,5 +22,13 @@ class Board
 
   def update_board(marker, position)
     @board[position] = marker
+    @win = [[@board[0], board[1], board[2]], [@board[3], board[4], board[5]],
+            [@board[6], board[7], board[8]], [@board[0], board[3], board[6]],
+            [@board[1], board[4], board[7]], [@board[2], board[5], board[8]],
+            [@board[0], board[4], board[8]], [@board[2], board[5], board[8]]]
+  end
+
+  def winner?
+    @win.any? { |combination| combination.uniq.length == 1 }
   end
 end
