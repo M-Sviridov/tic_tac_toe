c0b9d37117aa4c106c5959de460d05314aaf9324
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-05-16 13:17:50 +1000

    Refactor code to make it modular and check that marker is valid

--- player.rb
+++ player.rb
@@ -4,14 +4,18 @@
 class Player
   attr_reader :name, :marker
 
-  def initialize(name)
-    @name = name
+  @@num_of_players = 0 # rubocop:disable Style/ClassVars
+
+  def initialize
+    @@num_of_players += 1 # rubocop:disable Style/ClassVars
+    print "Player #{@@num_of_players}, enter your name: "
+    @name = gets.chomp
     loop do
       print 'Enter your marker (non-numeric): '
       @marker = gets.chomp
-      return @marker unless @marker.match?(/\d/)
+      return @marker unless @marker.match?(/\d/) || @marker.length != 1
 
-      puts 'Invalid input. Your marker must be non-numeric.'
+      puts 'Invalid input. Your marker must be non-numeric and length of 1.'
     end
   end
 end
